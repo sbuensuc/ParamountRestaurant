06c593c6d083ea06e5c5397c2e6561fb10d8bc1f
Sam <Sam@DESKTOP-O0S9A91>
2016-11-28 16:22:00 -0500

    Add project files.

---
+++ ParamountRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParamountRestaurant.Startup))]
+namespace ParamountRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
