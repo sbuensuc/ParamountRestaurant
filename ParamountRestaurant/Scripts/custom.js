c12dadda5250dd8bcbbea80144f45ab4948c2da6
Sam <Sam@DESKTOP-O0S9A91>
2016-11-28 22:54:46 -0500

    finished debugging main page

---
+++ ParamountRestaurant/Scripts/custom.js
@@ -0,0 +1,9 @@
+﻿//Custom JS code
+
+//JS to handle image modal
+$(function () {
+    $('.pop').on('click', function () {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+    });
+});
