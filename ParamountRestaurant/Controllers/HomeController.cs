30f8d61035ce19bf3c4c5392d741b312191bc423
Sam <Sam@DESKTOP-O0S9A91>
2016-11-29 03:08:44 -0500

    finished contact page and functionality

--- ParamountRestaurant/Controllers/HomeController.cs
+++ ParamountRestaurant/Controllers/HomeController.cs
@@ -22,7 +22,6 @@ namespace ParamountRestaurant.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
